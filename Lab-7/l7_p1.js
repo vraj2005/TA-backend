b7a2f903bc6b9d27c460ee7c358570864c9a6ef5
Vraj Nakum <vrajnakum57@gmail.com>
2025-12-14 20:44:06 +0530

    Lab-7 completed

--- Lab-7/l7_p1.js
+++ Lab-7/l7_p1.js
@@ -1,7 +1,13 @@
 const http=require('http')
 const server=http.createServer((req,resp)=>{
-    console.log("request Recived")
-    resp.end("Hello")
+
+    if (req.url === "/favicon.ico") {
+        resp.end();
+        return;
+    }
+
+    console.log("request Recived",req.url)
+    resp.end("Hello world in HTTP server")
 })
 server.listen(3000,()=>{
     console.log("Server started on port 3000");
--- Lab-7/l7_p3.js
+++ Lab-7/l7_p3.js
@@ -1,8 +1,8 @@
 const fs=require('fs')
 const http=require('http');
-// fs.writeFileSync('home.txt','This is home Page File')
-// fs.writeFileSync('about.txt','This is about Page File')
-// fs.writeFileSync('contact.txt','This is contact Page File')
+fs.writeFileSync('home.txt','This is home Page File')
+fs.writeFileSync('about.txt','This is about Page File')
+fs.writeFileSync('contact.txt','This is contact Page File')
 http.createServer((req, res) => {
     if (req.url === '/') {
         res.end(fs.readFileSync('home.txt'))
