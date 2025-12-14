cb7dca4870ec09385a6370c5f8c2bfe6583c9b01
Vraj Nakum <vrajnakum57@gmail.com>
2025-12-14 22:57:23 +0530

    Lab-7 completed

--- Lab-7/l7_p1.js
+++ Lab-7/l7_p1.js
@@ -12,3 +12,37 @@ const server=http.createServer((req,resp)=>{
 server.listen(3000,()=>{
     console.log("Server started on port 3000");
 })
+
+
+
+
+
+//if you want to create server using https
+// const https = require("https");
+// const fs = require("fs");
+
+// const options = {
+//     key: fs.readFileSync("key.pem"),
+//     cert: fs.readFileSync("cert.pem")
+// };
+
+// const server = https.createServer(options, (req, res) => {
+//     res.end("Hello Secure World");
+// });
+
+// server.listen(3000, () => {
+//     console.log("HTTPS server running on port 3000");
+// });
+
+
+
+
+// 📄 What are key.pem and cert.pem?
+
+// They are SSL certificate files:
+
+// key.pem → private key
+
+// cert.pem → public certificate
+
+// These are required to encrypt communication.
